2b2ed5c20c04e21f5dea498b69209f6aa3b2f2a5
Mark Cuckson <59055349+mcuckson@users.noreply.github.com>
2021-05-31 09:25:05 +0100

    x

---
+++ pages/_app.js
@@ -0,0 +1,82 @@
+import App from "next/app";
+import { TinaCMS, TinaProvider } from "tinacms";
+import {
+  GithubClient,
+  TinacmsGithubProvider,
+  GithubMediaStore,
+} from "react-tinacms-github";
+
+export default function Site(props) {
+  const github = new GithubClient({
+    proxy: "http://localhost:3001/api/proxy-github",
+    authCallbackRoute: "http://localhost:3001/api/create-github-access-token",
+    clientId: process.env.GITHUB_CLIENT_ID,
+    baseRepoFullName: process.env.REPO_FULL_NAME, // e.g: tinacms/tinacms.org,
+    baseBranch: process.env.BASE_BRANCH, // e.g. 'master' or 'main' on newer repos
+  });
+
+  /**
+   * 1. Create the TinaCMS instance
+   */
+  const cms = new TinaCMS({
+    enabled: !!props.pageProps.preview,
+    apis: {
+      /**
+       * 2. Register the GithubClient
+       */
+      github,
+    },
+    /**
+     * 3. Register the Media Store
+     */
+    media: new GithubMediaStore(github),
+    /**
+     * 4. Use the Sidebar and Toolbar
+     */
+    sidebar: false,
+    toolbar: true, //props.pageProps.preview,
+  });
+
+  const { Component, pageProps } = props;
+  return (
+    /**
+     * 5. Wrap the page Component with the Tina and Github providers
+     */
+    <TinaProvider cms={cms}>
+      <TinacmsGithubProvider
+        onLogin={onLogin}
+        onLogout={onLogout}
+        error={pageProps.error}
+      >
+        {/**
+         * 6. Add a button for entering Preview/Edit Mode
+         */}
+        {/* <EditLink cms={cms} /> */}
+        <Component {...pageProps} />
+      </TinacmsGithubProvider>
+    </TinaProvider>
+  );
+}
+
+const onLogin = async () => {
+  const token = localStorage.getItem("tinacms-github-token") || null;
+  const headers = new Headers();
+
+  if (token) {
+    headers.append("Authorization", "Bearer " + token);
+  }
+  /*  */
+  const resp = await fetch(`http://localhost:3001/api/preview`, {
+    headers: headers,
+  });
+  const data = await resp.json();
+
+  if (resp.status == 200) window.location.href = window.location.pathname;
+  else throw new Error(data.message);
+};
+
+const onLogout = () => {
+  return fetch(`http://localhost:3001/api/reset-preview`).then(() => {
+    window.location.reload();
+  });
+};
